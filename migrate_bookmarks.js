35a0452465aa6e49de13cdaa5337fb1da178ba97
Marneus68 <duane.bekaert@gmail.com>
2019-10-09 11:33:18 +0200

    Going trhough all nextUrls for every query, removed node_modules folder

--- migrate_bookmarks.js
+++ migrate_bookmarks.js
@@ -3,17 +3,27 @@ const PixivAppApi = require('pixiv-app-api')
 const pixiv = new PixivAppApi(process.env.NAME, process.env.PASSWORD)
 const pixiv_ = new PixivAppApi(process.env.NAME, process.env.PASSWORD)
 
+function bookmarkAction(bookmarks, first) {
+    bookmarks.illusts.forEach((f) => { 
+        pixiv.illustBookmarkAdd(f.id)
+            .then(() => console.log("Bookmarked " + f.title + " on " + first.user.name))
+            .catch((e) => console.log("Bookmarked " + f.title + " on " + first.user.name))
+    })
+    if (bookmarks.nextUrl) {
+        pixiv.fetch(bookmarks.nextUrl)
+            .then((bb) => {
+                bookmarkAction(bb, first);
+            })
+    }
+}
+
 pixiv.login(process.argv[2], process.argv[3])
     .then((first) => {
         pixiv_.login(process.argv[4], process.argv[5])
             .then((second) => {
                 pixiv_.userBookmarksIllust(second.user.id)
                     .then((bookmarks) => { 
-                        bookmarks.illusts.forEach((f) => { 
-                            pixiv.illustBookmarkAdd(f.id)
-                                .then(() => console.log("Bookmarked " + f.title + " on " + first.user.name))
-                                .catch((e) => console.log("Bookmarked " + f.title + " on " + first.user.name))
-                        })
+                        bookmarkAction(bookmarks, first);
                     })
             })
             .catch((e) => console.log(e.message))
--- migrate_follows.js
+++ migrate_follows.js
@@ -3,17 +3,27 @@ const PixivAppApi = require('pixiv-app-api')
 const pixiv = new PixivAppApi(process.env.NAME, process.env.PASSWORD)
 const pixiv_ = new PixivAppApi(process.env.NAME, process.env.PASSWORD)
 
+function followActions(follow, first) {
+    follow.userPreviews.forEach((f) => { 
+        pixiv.userFollowAdd(f.user.id)
+            .then(() => console.log("Now following " + f.user.name + " on " + first.user.name))
+            .catch((e) => console.log("Unable to follow " + f.user.name + " on " + first.user.name))
+    });
+    if (follow.nextUrl) {
+        pixiv.fetch(follow.nextUrl)
+            .then((ff) => {
+                followActions(ff, first);
+            });
+    }
+}
+
 pixiv.login(process.argv[2], process.argv[3])
     .then((first) => {
         pixiv_.login(process.argv[4], process.argv[5])
             .then((second) => {
                 pixiv_.userFollowing(second.user.id)
                     .then((follow) => { 
-                        follow.userPreviews.forEach((f) => { 
-                            pixiv.userFollowAdd(f.user.id)
-                                .then(() => console.log("Now following " + f.user.name + " on " + first.user.name))
-                                .catch((e) => console.log("Unable to follow " + f.user.name + " on " + first.user.name))
-                        })
+                        followActions(follow, first);
                     })
             })
             .catch((e) => console.log(e.message))
